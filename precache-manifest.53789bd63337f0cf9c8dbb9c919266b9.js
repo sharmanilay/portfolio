e83aa051a5bb561697b5c825082738a0e2f7d78e
Nilay Sharma <nilaysharma642@gmail.com>
2020-10-10 22:28:05 +0530

    Updates

---
+++ precache-manifest.53789bd63337f0cf9c8dbb9c919266b9.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c3efd1eb4b60458e3395888dceabcd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "578b7fa30aec45a15526",
+    "url": "/portfolio/static/css/3.bab16b3d.chunk.css"
+  },
+  {
+    "revision": "e6616333b0bd05fa0f53",
+    "url": "/portfolio/static/css/4.35e6824b.chunk.css"
+  },
+  {
+    "revision": "236b73858a2d46acb7b5",
+    "url": "/portfolio/static/css/main.5ab86466.chunk.css"
+  },
+  {
+    "revision": "40acd8983b33cdc19458",
+    "url": "/portfolio/static/js/2.89116058.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.89116058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "578b7fa30aec45a15526",
+    "url": "/portfolio/static/js/3.af575801.chunk.js"
+  },
+  {
+    "revision": "e6616333b0bd05fa0f53",
+    "url": "/portfolio/static/js/4.34157c6e.chunk.js"
+  },
+  {
+    "revision": "236b73858a2d46acb7b5",
+    "url": "/portfolio/static/js/main.f7d65dd7.chunk.js"
+  },
+  {
+    "revision": "a031666c7ca7f2948056",
+    "url": "/portfolio/static/js/runtime-main.11c11b7f.js"
+  },
+  {
+    "revision": "569e8718e1d1244cee61ab3dcd7f229d",
+    "url": "/portfolio/static/media/Home.569e8718.svg"
+  },
+  {
+    "revision": "7c8d5c080e63f6f6e665eb17926185dd",
+    "url": "/portfolio/static/media/arrow-down.7c8d5c08.svg"
+  },
+  {
+    "revision": "e069a7b2c27521dc63e58a8d91facda1",
+    "url": "/portfolio/static/media/arrow-right.e069a7b2.svg"
+  },
+  {
+    "revision": "38938965922ff4bc67a5db5103ba83d6",
+    "url": "/portfolio/static/media/chevron-right.38938965.svg"
+  },
+  {
+    "revision": "b58032a934007028100fb48d8466ccae",
+    "url": "/portfolio/static/media/close.b58032a9.svg"
+  },
+  {
+    "revision": "c1b25c4a9f857de38c8ae06c2fe34c53",
+    "url": "/portfolio/static/media/dribbble.c1b25c4a.svg"
+  },
+  {
+    "revision": "98256f25ebce65bedd17a79a7827388e",
+    "url": "/portfolio/static/media/dtt-large.98256f25.png"
+  },
+  {
+    "revision": "31c1a919983204a2f74afb7d3f2bd547",
+    "url": "/portfolio/static/media/dtt.31c1a919.png"
+  },
+  {
+    "revision": "6a087ef4ef0fc97e834c0897ce3d2419",
+    "url": "/portfolio/static/media/email.6a087ef4.svg"
+  },
+  {
+    "revision": "5e2aa24d176f00916fcf082cc7d2a024",
+    "url": "/portfolio/static/media/error.5e2aa24d.svg"
+  },
+  {
+    "revision": "c7f063e46425af2bd570874db62b0280",
+    "url": "/portfolio/static/media/figma.c7f063e4.svg"
+  },
+  {
+    "revision": "9d67ce004f11c63a6d47c9bb8ba6f7a6",
+    "url": "/portfolio/static/media/github.9d67ce00.svg"
+  },
+  {
+    "revision": "ce97af48ef4c7883c1570336ddb78d17",
+    "url": "/portfolio/static/media/gotham-book.ce97af48.woff2"
+  },
+  {
+    "revision": "12acaf395d9f94567db7089cf25c68c2",
+    "url": "/portfolio/static/media/gotham-medium.12acaf39.woff2"
+  },
+  {
+    "revision": "463cf85763b85ead5c58b6f6ebf09fb9",
+    "url": "/portfolio/static/media/linkedin.463cf857.svg"
+  },
+  {
+    "revision": "d74088ef3f0587871ecccedbdc2148ad",
+    "url": "/portfolio/static/media/menu.d74088ef.svg"
+  },
+  {
+    "revision": "ccbe1b93eeb10297639db55c649d799a",
+    "url": "/portfolio/static/media/modern-large.ccbe1b93.png"
+  },
+  {
+    "revision": "4f5a922410fb2af454f4c05adcb5b9f2",
+    "url": "/portfolio/static/media/modern.4f5a9224.png"
+  },
+  {
+    "revision": "4d98e081e82abc03f931e5a763f938cc",
+    "url": "/portfolio/static/media/mystgang-large.4d98e081.png"
+  },
+  {
+    "revision": "0b12c5821e729350f5f994bf60c49f09",
+    "url": "/portfolio/static/media/notfound.0b12c582.mp4"
+  },
+  {
+    "revision": "051afd6f747ad7256098605cfb490818",
+    "url": "/portfolio/static/media/pause.051afd6f.svg"
+  },
+  {
+    "revision": "87aaffab91eb0abc1ad02e103f921717",
+    "url": "/portfolio/static/media/pill-cover.87aaffab.jpg"
+  },
+  {
+    "revision": "01370b90ef2089e39de4033fe59a988a",
+    "url": "/portfolio/static/media/play.01370b90.svg"
+  },
+  {
+    "revision": "bd9ee12d6fce2f084b888a7795a0d635",
+    "url": "/portfolio/static/media/profile-large.bd9ee12d.png"
+  },
+  {
+    "revision": "5af0649394038b1103be1b35c61f8c08",
+    "url": "/portfolio/static/media/send.5af06493.svg"
+  },
+  {
+    "revision": "63ef9640e3c00bc17ff7af884b1b15e2",
+    "url": "/portfolio/static/media/stackoverflow.63ef9640.svg"
+  },
+  {
+    "revision": "4ea9273e168b5e4e69b16adc98cb08f3",
+    "url": "/portfolio/static/media/twitter.4ea9273e.svg"
+  }
+]);
